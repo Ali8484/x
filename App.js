28e0bbe66b4d078fff66b8a3ccc0e8af524e775d
github_ali8484 <you@domain.com>
2017-12-03 14:23:21 +0300

    'When_Wrong_Happened'

--- App.js
+++ App.js
@@ -1,39 +1,43 @@
-import React from 'react';
-import {Text , View , Button } from 'react-native';
+import React , {Component}  from 'react' ;
+import {AppRegistry, Text , View , Button } from 'react-native' ;
 import {StackNavigator} from 'react-navigation';
 
-const HomeScreen =({navigation}) => (
-  <View>
-    <Text>
-      This is the Home screen
-      </Text>
-      <Button 
-       onPress={() => navigation.navigate('Details')}
-       title='PRESS HERE'
-      />
-    </View>
-);
+export default class no extends Component{
+    render(){
+       return(
+//const HomeScreen =({navigation}) => (
+    <View>
+        <Text>
+        This is the Home maaan!
+            </Text>
+            <Button onPress={() => navigation.navigate('Details')}
+            title="PUSH IT"
+            />
+        </View>
+//);
+       );
+   }
+}
+AppRegistry.registerComponent('no' ,()=> no);
+
 const DetailsScreen =() => (
-  <View>
-    <Text>
-      This is the Details Screen
-      </Text>
-    </View>
+    <View>
+        <Text>
+            This is the Details screen
+            </Text>
+        </View>
 );
-
-const Root =StackNavigator ({
-  Home : {
-    screen : HomeScreen ,
-    navigationOptions:{
-      headerTitle : 'Homy',
+const Root= StackNavigator ({
+    Home : {
+        screen : HomeScreen ,
+        navigationOptions: {
+            headerTitle : "Homy "
+        },
     },
-  },
-
-  Details : {
-    screen : DetailsScreen ,
-    navigationOptions :{
-      headerTitle : 'Detaily'
+    Details : {
+        screen : DetailsScreen ,
+        navigationOptions: {
+            headerTitle : "Detaily "
+        },
     },
-  },
 });
-export default Root ;
